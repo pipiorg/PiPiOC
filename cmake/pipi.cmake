d7d40a6fcc6b8819ee5b40c2608135015782620c
CH-Chang <ch.chang.software@gmail.com>
2023-11-15 17:37:23 +0800

    feat: adjust fetch pipi from git repository

--- cmake/pipi.cmake
+++ cmake/pipi.cmake
@@ -1,11 +1,14 @@
 include(FetchContent)
 
-# TODO: FIX
-set(PIPI_SOURCE_DIR	/Users/chchang/Systex/TEMP/PiPi)
+set(PIPI_GIT_TAG	main)
+set(PIPI_GIT_URL	https://github.com/pipiorg/PiPi.git)
+set(PIPI_SOURCE_DIR	$(CMAKE_SOURCE_DIR)/ext/pipi)
 set(PIPI_BINARY_DIR	${CMAKE_SOURCE_DIR}/ext-build/pipi)
 
 FetchContent_Declare(
 	pipi
+	GIT_REPOSITORY	${PIPI_GIT_URL}
+	PIPI_GIT_TAG	${PIPI_GIT_TAG}
 	SOURCE_DIR		${PIPI_SOURCE_DIR}
 	BINARY_DIR		${PIPI_BINARY_DIR}
 )
